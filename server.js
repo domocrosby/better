1ec731f1f8d53df76b0337c57eb4e8f91060801b
Dominic Crosby <domcrosby1@gmail.com>
2016-11-30 19:53:10 +0000

    changed port to be dynamic

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express')
 var app = express()
 
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+app.listen(port, function () {
+  console.log('Example app listening on port '+port)
 })
+
