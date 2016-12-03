92c6e18f6a21a29a2d2c525dd2c792af48fd084b
Dominic Crosby <domcrosby1@gmail.com>
2016-12-03 01:52:08 +0000

    added pug templating language

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+var path = require('path');
 var express = require('express')
 var app = express()
+
+// set pug as template engine
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
 
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+app.get('/home',function(req,res){
+    res.render('index', {date: new Date().toDateString()})
 })
 
 app.listen(port, function () {
