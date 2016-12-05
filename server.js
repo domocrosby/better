497f076240f63d55f7265643828ca5e734c2b768
Dominic Crosby <domcrosby1@gmail.com>
2016-12-05 08:15:02 +0000

    added stylus to styling

--- server.js
+++ server.js
@@ -4,20 +4,19 @@ var app = express()
 
 // set pug as template engine
 app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', './views')
+
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.get('/home',function(req,res){
+app.get('/',function(req,res){
     res.render('index', {date: new Date().toDateString()})
 })
 
 app.listen(port, function () {
-  console.log('Example app listening on port '+port)
+  console.log('App listening on port '+port)
 })
 
