d1719a17206f4efa5d8d7e5f6c7c9d72fc49b289
Dominic Crosby <domcrosby1@gmail.com>
2017-01-30 02:28:08 +0000

    started clean up

--- public/main.js
+++ public/main.js
@@ -176,3 +176,19 @@ $(document).ready(function() {
    offscreen();
 
 });
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("adRail").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("adRail").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+}
+
+$('#adBtn').on( "click", function(){
+    openNav();
+})
