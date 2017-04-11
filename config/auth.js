e90c85d0b10bca62db47d2298afc25e3f72854b4
Dominic Crosby <domcrosby1@gmail.com>
2017-04-11 01:30:10 +0000

    fixed FB reference

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         clientID: '669090123216173' ,
         clientSecret: '3547aebe2d75fb865198205c35a6f795',
-        callbackURL: 'https://better-dominisi.c9users.io/auth/facebook/callback'
+        callbackURL: process.env.FB ||'https://better-dominisi.c9users.io/auth/facebook/callback'
     }
 
 };
